0def8bfd8bb78758799868368bd860c12746493b
denis-dityatkin-dev <59360590+denis-dityatkin-dev@users.noreply.github.com>
2023-05-28 14:53:15 +0300

    Add files via upload

---
+++ telegram-bot/src/main/java/com/example/telegrambot/service/VacancyService.java
@@ -0,0 +1,43 @@
+package com.example.telegrambot.service;
+
+import com.example.telegrambot.dto.VacancyDto;
+import jakarta.annotation.PostConstruct;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+@Service
+public class VacancyService {
+    @Autowired
+    private VacanciesReaderService vacanciesReaderService;
+    private final Map<String, VacancyDto> vacancies = new HashMap<>();
+    @PostConstruct
+    public void init(){
+        List<VacancyDto> list = vacanciesReaderService.getVacanciesFromFile("vacancies.csv");
+        for (VacancyDto vacancy: list) {
+            vacancies.put(vacancy.getId(), vacancy);
+    }
+
+    }
+    public List<VacancyDto> getJuniorVacancies(){
+    return vacancies.values().stream()
+            .filter(v -> v.getTitle().toLowerCase().contains("junior") )
+            .toList();
+    }
+    public List<VacancyDto> getMiddleVacancies(){
+        return vacancies.values().stream()
+                .filter(v -> v.getTitle().toLowerCase().contains("middle") )
+                .toList();
+    }
+    public List<VacancyDto> getSeniorVacancies(){
+        return vacancies.values().stream()
+                .filter(v -> v.getTitle().toLowerCase().contains("senior") )
+                .toList();
+    }
+    public VacancyDto get(String id){
+    return vacancies.get(id);
+    }
+}
